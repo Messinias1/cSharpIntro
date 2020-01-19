deb7f093615d47bf5a3b1f945f63da9215f2afd3
Carl Kakisis <Messinias1@gmail.com>
2020-01-18 20:45:09 -0500

    demo operators

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,16 +10,23 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            try
-            {
-                string str = "true";
-                bool b = Convert.ToBoolean(str);
-                Console.WriteLine(b);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("The number could not be converted to a byte");
-            }
+
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(c > b || c == a);
+
+            //try
+            //{
+            //    string str = "true";
+            //    bool b = Convert.ToBoolean(str);
+            //    Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("The number could not be converted to a byte");
+            //}
            
 
             //const float Pi = 3.14f;

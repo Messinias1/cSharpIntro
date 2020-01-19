3fe4a84bb2bc37a96780bce071fad490ff3229c0
Carl Kakisis <Messinias1@gmail.com>
2020-01-19 15:37:38 -0500

    demo enums and reference/value types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,26 +7,56 @@ using System;
 
 namespace HelloWorld
 {
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0} b: {1}", a, b));
 
-            var firstName = "Carl";
-            var lastName = "Kakisis";
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[0]: {0} array2[0]: {1}", array1[0], array2[0]));
 
-            var fullName = firstName + " " + lastName;
-            var mtFullName = string.Format("My name is {0} {1}", firstName, lastName);
+            // Enums
+            //var method = ShippingMethod.Express;
+            //Console.WriteLine((int)method);
 
-            var names = new string[3] { "John", "Bob", "Bev" };
-            var formattedNames = string.Join(",", names);
-            Console.WriteLine(formattedNames);
+            //var methodId = 3;
+            //Console.WriteLine((ShippingMethod)methodId);
 
-            var text = @"What is your name?
-My name is Carl, yours?
-Bob thanks";
-            Console.WriteLine(text);
+            //Console.WriteLine(method.ToString());
 
+            //var methodName = "Express";
+            //var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            //Console.WriteLine(shippingMethod);
+
+            // Strings
+//            var firstName = "Carl";
+//            var lastName = "Kakisis";
+
+//            var fullName = firstName + " " + lastName;
+//            var mtFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+//            var names = new string[3] { "John", "Bob", "Bev" };
+//            var formattedNames = string.Join(",", names);
+//            Console.WriteLine(formattedNames);
+
+//            var text = @"What is your name?
+//My name is Carl, yours?
+//Bob thanks";
+//            Console.WriteLine(text);
+
+            // Arrays
             //var numbers = new int[3];
             //numbers[0] = 1;
 
@@ -40,10 +70,12 @@ Bob thanks";
             //Console.WriteLine(flags[0]);
             //Console.WriteLine(flags[1]);
             //Console.WriteLine(flags[2]);
-
+     
             //var names = new string[3] { "Javk", "John", "Mary" };
             //Console.WriteLine(names[0]);
 
+
+            // Classes
             //var john = new Person();
             //john.FirstName = "John";
             //john.LastName = "Smith";
@@ -59,6 +91,7 @@ Bob thanks";
 
             //Console.WriteLine(c > b || c == a);
 
+            // implcit and explicit conversion
             //try
             //{
             //    string str = "true";

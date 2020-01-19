2504541b2ccd7ff91678e4bc1090f965e3650708
Carl Kakisis <Messinias1@gmail.com>
2020-01-18 20:28:49 -0500

    demp type conversions

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,8 +10,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            const float Pi = 3.14f;
-            Console.WriteLine(Pi);
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
+           
+
+            //const float Pi = 3.14f;
+            //Console.WriteLine(Pi);
         }
     }
 }

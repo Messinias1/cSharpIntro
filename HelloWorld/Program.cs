5e3f5af50dde8d7b08f4639dd31167a41cc2cfa9
Carl Kakisis <Messinias1@gmail.com>
2020-01-19 21:24:08 -0500

    loops demo

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,21 +11,53 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            for (var i = 0; i <= 10; i++)
+            while (true)
             {
-                if(i % 2 == 0)
+                Console.Write("Type your name");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
                 }
+                break;
             }
 
-            for (var i = 10; i >= 1; i--)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
+            //var numbers = new int[] { 1, 2, 3 };
+
+            //foreach(var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //var name = "John Smith";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach (var character in name) 
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //for (var i = 0; i <= 10; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //for (var i = 10; i >= 1; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
 
             // VIDEO 42 EXERCISES
             // X 42 1

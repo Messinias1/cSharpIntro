fe661fb8cefd749e23eec0596ade9dd934f90486
Carl Kakisis <Messinias1@gmail.com>
2020-01-23 13:16:01 -0500

    first duplicate algo

---
+++ HelloWorld/Algos/Algos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Algos
+{
+    class Algos
+    {
+        int firstDuplicate(int[] a)
+        {
+            if (a.Length == a.Distinct().Count())
+                return -1;
+
+            HashSet<int> hs = new HashSet<int>();
+            foreach (var item in a)
+                if (!hs.Add(item))
+                    return item;
+            return -1;
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,21 +11,33 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Time Span
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            Console.WriteLine("Minute: " + timeSpan.Minutes);
+
             // Date Time
-            var dateTime = new DateTime(2015, 1, 1);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
+            //var dateTime = new DateTime(2015, 1, 1);
+            //var now = DateTime.Now;
+            //var today = DateTime.Today;
 
-            Console.WriteLine("Hour: " + now.Hour);
-            Console.WriteLine("Minute: " + now.Minute);
+            //Console.WriteLine("Hour: " + now.Hour);
+            //Console.WriteLine("Minute: " + now.Minute);
 
-            var tomorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
+            //var tomorrow = now.AddDays(1);
+            //var yesterday = now.AddDays(-1);
 
-            Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.ToShortDateString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToString("yyyy-MM-dd"));
+            //Console.WriteLine(now.ToLongDateString());
+            //Console.WriteLine(now.ToShortDateString());
+            //Console.WriteLine(now.ToLongTimeString());
+            //Console.WriteLine(now.ToString("yyyy-MM-dd"));
 
             // X 55 1
             //var inputList = new List<string>();

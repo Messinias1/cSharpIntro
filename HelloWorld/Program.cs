17f5c6287f56b0f9ab0cc9660c3e0848ebf5bca6
Carl Kakisis <Messinias1@gmail.com>
2020-01-25 01:05:19 -0500

    full demo strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,9 +12,33 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Demo Strings
-            var fullName = "Carl Kakisis ";
-            Console.WriteLine("Trim: '{0}'", fullName.Trim());
-            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+            //var fullName = "Carl Kakisis ";
+            //Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            //Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            //var index = fullName.IndexOf(' ');
+            //var firstName = fullName.Substring(0, index);
+            //var lastName = fullName.Substring(index + 1);
+            //Console.WriteLine("first name: " + firstName);
+            //Console.WriteLine("last name: " + lastName);
+
+            //var names = fullName.Split(' ');
+            //Console.WriteLine("First Name: " + names[0]);
+            //Console.WriteLine("Last Name: " + names[1]);
+
+            //Console.WriteLine(fullName.Replace("Kakisis", "östman"));
+
+            //if(String.IsNullOrWhiteSpace(" "))
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+
+            //var str = "25";
+            //var age = Convert.ToByte(str);
+            //Console.WriteLine(age);
+
+            //float price = 29.95f;
+            //Console.WriteLine(price.ToString("C"));
 
 
             // Time Span
